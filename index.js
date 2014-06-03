e658d3c422dcb87bcb768024c33f31c9db8c04ee
Jose Miralles <magicomiralles@gmail.com>
2014-06-03 21:51:13 +0000

    Changed index.js to listen on 8080

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express') /**/
-var app = express();
+var express = require('express');
 var fs = require('fs');
+var htmlfile = 'index.html';
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  var data = fs.readFileSync('index.html').toString();
-  response.send(data);
-})
+app.get('/',function(request,response){
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+    console.log('Listening on '+port);
+});
